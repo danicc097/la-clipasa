bdbace981d729765bc6a9a95e365082ad0c8841f
Daniel Castro <danicc097@gmail.com>
2022-12-30 19:29:59 +0000

    add twitch login

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # discord-twitch-webapp-test
+
+
+## TODO
+
+- ssr on edge vs separate spa deployment
+- https://vercel.com/docs/concepts/monorepos in case of separate spa
---
+++ backend/edge-functions/pages/api/auth/twitch/callback.ts
@@ -0,0 +1,15 @@
+import { NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+export default async (request: NextRequest, response: any) => {
+  try {
+    return NextResponse.redirect(process.env.BASE_URL ?? '/')
+  } catch (error: any) {
+    console.log(JSON.stringify(error));
+    return new Response(JSON.stringify(error?.message));
+  }
+};
