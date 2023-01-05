ccfd912dd58046b8a19138a61744762d566e6646
Daniel Castro <danicc097@gmail.com>
2023-01-05 21:41:14 +0000

    symlinks

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
   extends: ["custom"],
   settings: {
     next: {
-      rootDir: ["backend", "frontend", "packages/database"],
+      rootDir: ["backend", "frontend", "packages/*/"],
     },
   },
 };
