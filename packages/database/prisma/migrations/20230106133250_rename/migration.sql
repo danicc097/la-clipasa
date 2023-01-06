befad3f0d00c45db2c0d8a8678d0ee1ede9fe474
Daniel Castro <danicc097@gmail.com>
2023-01-06 13:34:15 +0000

    test dummy migration changes

---
+++ packages/database/prisma/migrations/20230106133250_rename/migration.sql
@@ -0,0 +1,15 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `isSubscriber` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[dummyField]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `dummyField` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."User" DROP COLUMN "isSubscriber",
+ADD COLUMN     "dummyField" TEXT NOT NULL,
+ADD COLUMN     "renameIsSubscriber" BOOLEAN NOT NULL DEFAULT false;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_dummyField_key" ON "public"."User"("dummyField");
