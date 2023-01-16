2df6c9115920dfb3bd1d94ec8eaf9bf72a47c34d
Daniel Castro <danicc097@gmail.com>
2023-01-16 10:07:16 +0000

    generic date parsing

--- frontend/src/queries/interceptors.ts
+++ frontend/src/queries/interceptors.ts
@@ -2,8 +2,9 @@ import type axios from 'axios'
 import type { AxiosRequestConfig, AxiosResponse } from 'axios'
 import dayjs from 'dayjs'
 
+// not needed right now
 export const requestInterceptor = (config: AxiosRequestConfig) => {
-  config.data = updateTimestamps(config.data)
+  // config.data = updateTimestamps(config.data)
   return config
 }
 
@@ -12,7 +13,9 @@ export const responseInterceptor = (response: AxiosResponse) => {
   return response
 }
 
-// TODO genereic solution: https://weblog.west-wind.com/posts/2014/jan/06/javascript-json-date-parsing-and-real-dates
+const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d{1,}|)Z$/
+
+// TODO generic solution: https://weblog.west-wind.com/posts/2014/jan/06/javascript-json-date-parsing-and-real-dates
 export const updateTimestamps = (obj: any, depth = 0) => {
   if (depth > 3) return
   if (Array.isArray(obj)) {
@@ -23,7 +26,7 @@ export const updateTimestamps = (obj: any, depth = 0) => {
     })
   } else {
     for (const key in obj) {
-      if (key === 'createdAt' || key === 'updatedAt') {
+      if (dateFormat.test(obj[key])) {
         obj[key] = dayjs(obj[key]).toDate()
       }
       if (typeof obj[key] === 'object') {
